9dc234de352d88a5599552c2a2daafae44e50d83
shiba <shibariku0623@icloud.com>
2025-02-08 04:29:14 +0900

    slack_notify.ymlの変更

--- bot.py
+++ bot.py
@@ -85,7 +85,7 @@ def notify_articles_to_slack(channel_id, api_token, tag='AI'):
 
 
 # 毎日8:30に実行
-schedule.every().day.at("04:17").do(lambda: notify_articles_to_slack(CHANNEL_ID, API_TOKEN))
+schedule.every().day.at("04:30").do(lambda: notify_articles_to_slack(CHANNEL_ID, API_TOKEN))
 
 
 while True:
